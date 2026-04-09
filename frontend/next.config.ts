17367c6a90c05b485250b131dce04fbafd35ecc4
Lucianodp74 <lucianodelpriore74@gmail.com>
2026-04-09 07:08:07 +0200

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: 'standalone',   // necessario per Docker
 }
 
 export default nextConfig
